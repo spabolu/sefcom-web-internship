89329d04b9b4f1668fe37f59a41df47063373770
Saketh <51687055+spabolu@users.noreply.github.com>
2021-07-18 17:16:10 -0700

    added mutationobserver and html body

---
+++ app.js
@@ -0,0 +1,21 @@
+// Select the node that will be observed for mutations
+const targetNode = document.getElementById('root');
+
+// Callback function to execute when mutations are observed
+const callback = function(mutationsList, observer) {
+    // Use traditional 'for loops' for IE 11
+    for(const mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+            console.log('A child node has been added or removed.');
+        }
+        else if (mutation.type === 'attributes') {
+            console.log('The ' + mutation.attributeName + ' attribute was modified.');
+        }
+    }
+};
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, {subtree: true, childList: true});
